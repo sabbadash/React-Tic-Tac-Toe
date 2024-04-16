852fe4d8585f5000c5e073d6bedeb865cd8d524a
Nazar Sabadash <nazarii.sabadash@gmail.com>
2024-04-16 14:14:57 +0100

    Update Player Component Functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEdit() {
@@ -8,15 +9,26 @@ export default function Player({ name, symbol }) {
     setIsEditing((prevState) => !prevState); //good practice!
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleNameChange}
+      />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
